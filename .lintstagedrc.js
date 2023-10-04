b575adfe13e0dc7462e35f32e312cd786d98f19c
reisblucas <lucasb.reis@hotmail.com>
2023-10-04 16:52:03 -0300

    fix(lint-staged): need to run only on stage files instead of all

---
+++ .lintstagedrc.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --max-warnings=0 --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+
+module.exports = {
+  '**/*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write --ignore-unknown'],
+  '**/*.json': 'prettier --write --ignore-unknown',
+}
