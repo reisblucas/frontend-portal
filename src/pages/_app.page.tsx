ec2905323eb8430eb57ebc43fa142f3ab94d7249
reisblucas <lucasb.reis@hotmail.com>
2023-10-04 16:40:18 -0300

    feat(tanstack): add queryclient provider on app

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app'
 
 import { ChakraCustomProvider } from '@/infra/chakra/provider'
+import { QueryClientProvider } from '@/infra/tanstack'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraCustomProvider>
-      <Component {...pageProps} />
+      <QueryClientProvider>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </ChakraCustomProvider>
   )
 }
