b952a8ea2aae9728fc9955dbaf44330c875002d8
reisblucas <lucasb.reis@hotmail.com>
2023-10-05 20:29:15 -0300

    feat(infra/axios): add interceptor on header to send Bearer on request to external api

--- src/infra/axios/auth.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-export const auth = axios.create({
-  url: '/api/auth',
-})
--- src/infra/axios/client.ts
+++ src/infra/axios/client.ts
@@ -1,5 +1,16 @@
 import axios from 'axios'
+import { getSession } from 'next-auth/react'
 
 export const client = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+})
+
+client.interceptors.request.use(async (config) => {
+  const session = await getSession()
+
+  if (session && session.clientToken) {
+    config.headers.Authorization = `Bearer ${session.clientToken}`
+  }
+
+  return config
 })
