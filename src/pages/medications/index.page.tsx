d525c1a0f342f4fcbe2ffdd26109ae1c07b7db75
reisblucas <lucasb.reis@hotmail.com>
2023-10-06 16:39:38 -0300

    fix(medications): adjust grid list to apply wordwrap and overflow

--- src/pages/medications/index.page.tsx
+++ src/pages/medications/index.page.tsx
@@ -1,9 +1,41 @@
 import { Medication, useMedications } from '@/medications'
-import { Flex, Grid, Heading, Skeleton, Text, VStack } from '@chakra-ui/react'
-import React, { useCallback } from 'react'
+import { Flex, Grid, Heading, Link, Skeleton, Text, VStack } from '@chakra-ui/react'
+import React, { useCallback, useState } from 'react'
 
 export default function Medications() {
-  const medications = useMedications({ page: 1, limit: 50 })
+  const medications = useMedications({ page: 5, limit: 50 })
+  const [crrPage] = useState(1) // after that get actual page using route params to inject on state
+
+  const getPagination = useCallback(
+    (last_page: number, pagesVisible = 1) => {
+      const pages: Medications.PaginationPages[] = []
+
+      for (let i = 1; i <= last_page; i++) {
+        const obj = { page: i, show: false }
+
+        if (i === 1 || i === last_page) {
+          obj.show = true
+        }
+
+        if (crrPage === i) {
+          obj.show = true
+        }
+
+        if (i === crrPage - pagesVisible || i === crrPage + pagesVisible) {
+          obj.show = true
+        }
+
+        if (i > crrPage - pagesVisible && i < crrPage + pagesVisible) {
+          obj.show = true
+        }
+
+        pages.push(obj)
+      }
+
+      return pages
+    },
+    [crrPage],
+  )
 
   const formatStrength = useCallback((strength: string) => {
     const asterisk = strength.indexOf('*')
@@ -17,7 +49,7 @@ export default function Medications() {
       return medication.strength.includes('**') ? (
         <>
           <Text fontSize={{ base: '2xs', md: 'xs' }}>{formatStrength(medication.strength).strength}</Text>
-          <Text fontSize={{ base: '2xs', md: 'xs' }}>{formatStrength(medication.strength).observation}</Text>
+          {/* <Text fontSize={{ base: '2xs', md: 'xs' }}>{formatStrength(medication.strength).observation}</Text> */}
         </>
       ) : (
         <Text fontSize={{ base: '2xs', md: 'xs' }}>{medication.strength}</Text>
@@ -34,9 +66,10 @@ export default function Medications() {
 
       <Skeleton isLoaded={medications.isSuccess}>
         <Grid
-          gridTemplateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }}
+          gridTemplateColumns={{ base: 'repeat(1, 1fr)', md: '50% 50%', lg: 'repeat(3, 1fr)' }}
           gridAutoFlow="dense"
           gap={8}
+          rowGap={10}
         >
           {medications.isSuccess &&
             medications.data.data.map((medication) => {
@@ -46,40 +79,60 @@ export default function Medications() {
                   flexDirection="column"
                   padding={2}
                 >
-                  <Text fontSize={{ base: 'xs', md: 'md' }} textAlign="center" fontWeight="bold">{`${
-                    medication.drug_name
-                  } ${formatStrength(medication.strength).strength}`}</Text>
+                  <Text
+                    fontSize={{ base: 'xs', md: 'md' }}
+                    textAlign="center"
+                    fontWeight="bold"
+                  >{`${medication.drug_name} `}</Text>
 
-                  <Grid gridTemplateColumns="100px 1fr">
-                    <Flex
-                      flexDirection="column"
-                      gap={2}
-                      sx={{
-                        p: {
+                  <Grid
+                    gridTemplateColumns="100px 1fr"
+                    sx={{
+                      p: {
+                        '#title': {
                           fontWeight: '600',
                         },
-                      }}
-                    >
-                      <Text variant="grid">Active ing.:</Text>
-                      <Text variant="grid">App num.:</Text>
-                      <Text variant="grid">Product num.:</Text>
-                      <Text variant="grid">Form:</Text>
-                      <Text variant="grid">Strength:</Text>
-                    </Flex>
-
-                    <Flex gap={2} flexDirection="column">
-                      <Text variant="grid">{medication.active_ingredient}</Text>
-                      <Text variant="grid">{medication.application_number}</Text>
-                      <Text variant="grid">{medication.product_number}</Text>
-                      <Text variant="grid">{medication.form}</Text>
-                      {getStrength(medication)}
-                    </Flex>
+                      },
+                    }}
+                  >
+                    <Text variant="grid">Active ing.:</Text>
+                    <Text variant="grid" noOfLines={1}>
+                      {medication.active_ingredient}
+                    </Text>
+                    <Text variant="grid">App num.:</Text>
+                    <Text variant="grid">{medication.application_number}</Text>
+                    <Text variant="grid">Product num.:</Text>
+                    <Text variant="grid">{medication.product_number}</Text>
+                    <Text variant="grid">Form:</Text>
+                    <Text variant="grid">{medication.form}</Text>
+                    <Text variant="grid">Strength:</Text>
+                    {getStrength(medication)}
                   </Grid>
                 </Flex>
               )
             })}
         </Grid>
+
+        {medications.isSuccess &&
+          getPagination(medications.data.last_page, 2).map((page) => {
+            if (!page.show) {
+              return
+            }
+
+            return (
+              <Flex gap={2} key={page.page * 3.14}>
+                <Link href={`/medications/${page.page}`}>{page.page}</Link>
+              </Flex>
+            )
+          })}
       </Skeleton>
     </VStack>
   )
+}
+
+export namespace Medications {
+  export interface PaginationPages {
+    page: number
+    show: boolean
+  }
 }
