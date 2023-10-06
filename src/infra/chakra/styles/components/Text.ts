f89690ffa14f39f53f971a99ee247d094c8ba1cd
reisblucas <lucasb.reis@hotmail.com>
2023-10-06 14:04:56 -0300

    feat(chakra/style): add text variant on infra

---
+++ src/infra/chakra/styles/components/Text.ts
@@ -0,0 +1,9 @@
+import { ComponentStyleConfig } from '@chakra-ui/react'
+
+export const Text: ComponentStyleConfig = {
+  variants: {
+    grid: {
+      fontSize: { base: '2xs', md: 'xs' },
+    },
+  },
+}
