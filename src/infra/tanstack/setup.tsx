bb90f82a97d40c184623a72fb7bc1d3d5d559e38
reisblucas <lucasb.reis@hotmail.com>
2023-10-05 21:03:38 -0300

    feat: set to false refecth on window focus

--- src/infra/tanstack/setup.tsx
+++ src/infra/tanstack/setup.tsx
@@ -1,7 +1,13 @@
 import { QueryClient } from '@tanstack/react-query'
 import { QueryClientProvider as Provider } from '@tanstack/react-query'
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 export function QueryClientProvider({ children }: QueryClientProvider.Props) {
   return <Provider client={queryClient}>{children}</Provider>
