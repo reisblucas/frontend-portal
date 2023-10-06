ddbaeec2d6ab0dca4bc66361d1a9ac4fe9958e96
reisblucas <lucasb.reis@hotmail.com>
2023-10-06 13:43:28 -0300

    feat: add initial medications list

--- src/pages/medications/index.page.tsx
+++ src/pages/medications/index.page.tsx
@@ -1,12 +1,66 @@
-import { useMedications } from '@/medications'
-import { useSession } from 'next-auth/react'
-import React from 'react'
+import { Medication, useMedications } from '@/medications'
+import { Flex, Grid, Heading, Skeleton, Text, VStack } from '@chakra-ui/react'
+import React, { useCallback } from 'react'
 
 export default function Medications() {
-  const medications = useMedications()
-  const session = useSession()
-  console.log('session', session)
-  console.log('medications', medications.data)
+  const medications = useMedications({ page: 1, limit: 50 })
 
-  return <div>Medications</div>
+  const formatStrength = useCallback((strength: string) => {
+    const asterisk = strength.indexOf('*')
+    const strengthText = strength.slice(0, asterisk - 1)
+    const observation = strength.slice(asterisk)
+
+    return { strength: strengthText, observation }
+  }, [])
+  const getStrength = useCallback(
+    (medication: Medication) => {
+      return medication.strength.includes('**') ? (
+        <>
+          <Text fontSize={{ base: '2xs', md: 'xs' }}>{formatStrength(medication.strength).strength}</Text>
+          <Text fontSize={{ base: '2xs', md: 'xs' }}>{formatStrength(medication.strength).observation}</Text>
+        </>
+      ) : (
+        <Text fontSize={{ base: '2xs', md: 'xs' }}>{medication.strength}</Text>
+      )
+    },
+    [formatStrength],
+  )
+
+  return (
+    <VStack gap={4}>
+      <Heading as="h1" textAlign="center">
+        Medications
+      </Heading>
+
+      <Skeleton isLoaded={medications.isSuccess}>
+        <Grid
+          gridTemplateColumns={{ base: 'repeat(2, 1fr)', md: 'repeat(4, 1fr)' }}
+          gridAutoFlow="dense"
+          gap={4}
+          // justifyContent="space-around"
+        >
+          {medications.isSuccess &&
+            medications.data.data.map((medication) => {
+              return (
+                <Flex
+                  key={medication.application_number + medication.product_number}
+                  flexDirection="column"
+                  padding={2}
+                >
+                  <Text fontSize={{ base: 'xs', md: 'md' }}>{`${medication.drug_name} ${medication.strength}`}</Text>
+
+                  <Flex gap={2} flexDirection="column">
+                    <Text fontSize={{ base: '2xs', md: 'xs' }}>{medication.active_ingredient}</Text>
+                    <Text fontSize={{ base: '2xs', md: 'xs' }}>{medication.application_number}</Text>
+                    <Text fontSize={{ base: '2xs', md: 'xs' }}>{medication.product_number}</Text>
+                    <Text fontSize={{ base: '2xs', md: 'xs' }}>{medication.form}</Text>
+                    {getStrength(medication)}
+                  </Flex>
+                </Flex>
+              )
+            })}
+        </Grid>
+      </Skeleton>
+    </VStack>
+  )
 }
