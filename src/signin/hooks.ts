0e18ec916ede3482248f30a5d4e07ea98edb33d0
reisblucas <lucasb.reis@hotmail.com>
2023-10-09 08:16:31 -0300

    refactor(hooks): push route after login

--- src/signin/hooks.ts
+++ src/signin/hooks.ts
@@ -1,11 +1,14 @@
+import { useRouter } from 'next/router'
 import { signinService } from './services'
 import { useMutation } from '@tanstack/react-query'
 
 export const useLogin = () => {
+  const router = useRouter()
+
   return useMutation({
     mutationFn: signinService.login,
     onSuccess: () => {
-      console.log('gonna redirect to the list page')
+      router.push('/medications')
     },
   })
 }
