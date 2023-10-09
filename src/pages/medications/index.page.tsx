a7749ec8653fb5e88a38a9f901df67a46c57b9e9
reisblucas <lucasb.reis@hotmail.com>
2023-10-09 17:34:14 -0300

    refactor(page): use pagination hook and medicationsgridlist on page

--- src/pages/medications/index.page.tsx
+++ src/pages/medications/index.page.tsx
@@ -1,9 +1,8 @@
-import { Flex, Grid, Heading, Input, Select, Skeleton, Text, VStack } from '@chakra-ui/react'
-import Link from 'next/link'
+import { Button, Flex, Heading, Input, Select, Skeleton, Text, VStack } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import React, { useCallback, useEffect, useMemo, useState } from 'react'
 
-import { Medication, useMedications } from '@/medications'
+import { MedicationsGridList, useMedications, usePagination } from '@/medications'
 import { useDebounce, useDeleteRouterQuery, useUpdateRouterQuery } from '@/infra/helpers'
 
 export default function Medications() {
@@ -46,12 +45,18 @@ export default function Medications() {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [debouncedInputValue, search])
+
   useEffect(() => {
     if (limit !== null) {
       updateRouterQuery('limit', limit.toString())
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [limit])
+
+  useEffect(() => {
+    updateRouterQuery('page', crrPage.toString())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [crrPage])
 
   const shouldUseSearchIf = querySearch
     ? querySearch
@@ -64,75 +69,17 @@ export default function Medications() {
     search: shouldUseSearchIf,
   })
 
+  const last_page = medications.isSuccess ? medications.data.last_page : 1
+  const pagination = usePagination({ crrPage, last_page, pagesVisible: 2 })
+
+  useEffect(() => {
+    if (Number(router.query.page) > medications.data?.last_page) {
+      setCrrPage(1)
+    }
+  }, [medications.data, router.query.page, search])
+
   const showNoComponentData =
     medications.isSuccess && medications.data.data.length === 0 && search !== null && !medications.isFetching
-
-  const getPagination = useCallback(
-    (last_page: number, pagesVisible = 1) => {
-      let setEllipsisNow = false
-      const pages: Medications.PaginationPages[] = []
-
-      for (let i = 1; i <= last_page; i++) {
-        const obj = { page: i, show: false }
-
-        if (i === 1 || i === last_page) {
-          obj.show = true
-        }
-
-        if (crrPage === i) {
-          obj.show = true
-        }
-
-        if (i === crrPage - pagesVisible || i === crrPage + pagesVisible) {
-          obj.show = true
-        }
-
-        if (i > crrPage - pagesVisible && i < crrPage + pagesVisible) {
-          obj.show = true
-        }
-
-        if (obj.show) {
-          pages.push(obj)
-
-          if (!setEllipsisNow) {
-            setEllipsisNow = true
-            continue
-          } else {
-            continue
-          }
-        }
-
-        if (setEllipsisNow) {
-          pages.push({ page: i, show: false })
-          setEllipsisNow = false
-        }
-      }
-
-      return pages
-    },
-    [crrPage],
-  )
-
-  const formatStrength = useCallback((strength: string) => {
-    const asterisk = strength.indexOf('*')
-    const strengthText = strength.slice(0, asterisk - 1)
-    const observation = strength.slice(asterisk)
-
-    return { strength: strengthText, observation }
-  }, [])
-
-  const getStrength = useCallback(
-    (medication: Medication) => {
-      return medication.strength.includes('**') ? (
-        <Text fontSize={{ base: '2xs', md: 'xs' }}>{formatStrength(medication.strength).strength} **</Text>
-      ) : (
-        <Text fontSize={{ base: '2xs', md: 'xs' }} noOfLines={1}>
-          {medication.strength}
-        </Text>
-      )
-    },
-    [formatStrength],
-  )
 
   const handleSearchAction = useCallback((e: React.FormEvent<HTMLInputElement>) => {
     const {
@@ -192,82 +139,25 @@ export default function Medications() {
           </Text>
         )}
 
-        <Grid
-          gridTemplateColumns={{ base: 'repeat(1, 1fr)', md: '50% 50%', lg: 'repeat(3, 1fr)' }}
-          gridAutoFlow="dense"
-          gap={8}
-          rowGap={10}
-        >
-          {medications.isSuccess &&
-            medications.data.data.map((medication) => {
-              return (
-                <Flex
-                  key={medication.application_number + medication.product_number}
-                  flexDirection="column"
-                  padding={2}
-                >
-                  <Text
-                    fontSize={{ base: 'xs', md: 'md' }}
-                    textAlign="center"
-                    fontWeight="bold"
-                  >{`${medication.drug_name} `}</Text>
+        {medications.isSuccess && <MedicationsGridList medications={medications.data} />}
 
-                  <Grid
-                    gridTemplateColumns="100px 1fr"
-                    sx={{
-                      p: {
-                        '#title': {
-                          fontWeight: '600',
-                        },
-                      },
-                    }}
-                  >
-                    <Text variant="grid">Active ing.:</Text>
-                    <Text variant="grid" noOfLines={1}>
-                      {medication.active_ingredient}
-                    </Text>
-                    <Text variant="grid">App num.:</Text>
-                    <Text variant="grid">{medication.application_number}</Text>
-                    <Text variant="grid">Product num.:</Text>
-                    <Text variant="grid">{medication.product_number}</Text>
-                    <Text variant="grid">Form:</Text>
-                    <Text variant="grid">{medication.form}</Text>
-                    <Text variant="grid">Strength:</Text>
-                    {getStrength(medication)}
-                  </Grid>
-                </Flex>
-              )
-            })}
-        </Grid>
-
-        <Flex gap={2} justifyContent="center">
+        <Flex gap={2} justifyContent="center" alignItems="center">
           {medications.isSuccess &&
-            getPagination(medications.data.last_page, 2).map(({ page, show }) => {
+            pagination().map(({ page, show }) => {
               if (!show) {
                 return <Text key={`${page * Math.PI} ${show}`}>...</Text>
               }
 
               return (
-                <Link
-                  href={{ pathname: '/medications', query: { ...router.query, page } }}
-                  key={`${page * Math.PI} ${show}`}
-                  onClick={() => setCrrPage(page)}
-                >
+                <Button key={`${page * Math.PI} ${show}`} onClick={() => setCrrPage(page)} variant="outline">
                   <Text color={queryPage === page ? 'green.500' : 'inherit'} _hover={{ color: 'blue.500' }}>
                     {page}
                   </Text>
-                </Link>
+                </Button>
               )
             })}
         </Flex>
       </Skeleton>
     </VStack>
   )
-}
-
-export namespace Medications {
-  export interface PaginationPages {
-    page: number
-    show: boolean
-  }
 }
