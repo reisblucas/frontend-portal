7344c46ae751718dc5a6b0e9791d54454a1f0b4e
reisblucas <lucasb.reis@hotmail.com>
2023-10-10 09:18:53 -0300

    style(header): change position from relative to sticky

--- src/ui/Layout/components/Header.tsx
+++ src/ui/Layout/components/Header.tsx
@@ -8,9 +8,12 @@ export function Header() {
     <Flex
       justifyContent="center"
       minHeight="50px"
+      minWidth="full"
       alignItems="center"
-      position="relative"
       boxShadow="rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;"
+      position="sticky"
+      top={0}
+      backgroundColor="white"
     >
       <Flex
         sx={{
