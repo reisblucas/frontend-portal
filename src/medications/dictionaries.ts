e1a11e4e1765ff1fedd32e951c55133fee2c373b
reisblucas <lucasb.reis@hotmail.com>
2023-10-09 22:43:38 -0300

    feat(dict): add medication dictionary based on creation fields

---
+++ src/medications/dictionaries.ts
@@ -0,0 +1,7 @@
+export const MedicationFieldDictionary = {
+  drug_name: 'Drugs name:',
+  units_per_package: 'Units per package:',
+  issued_on: 'Issued on:',
+  expires_on: 'Expires on:',
+  manufacturers: 'Manufacturers:',
+}
